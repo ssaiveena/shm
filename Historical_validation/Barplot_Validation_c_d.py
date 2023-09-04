314064140d020bbb1ed470eb92dbd33cd1024dec
Sai Veena <40389169+ssaiveena@users.noreply.github.com>
2023-09-04 15:16:09 -0700

    Update Barplot_Validation_c_d.py

--- Historical_validation/Barplot_Validation_c_d.py
+++ Historical_validation/Barplot_Validation_c_d.py
@@ -1,48 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# data to plot
-# n_groups = 3
-# means_obs = (1.03, 0.12, 3.56)
-# means_shm2 = (6.08, 3.02, 26.07)
-# means_shm3 = (6.08, 3.02, 24.82)
-#
-# # create plot
-# fig, ax = plt.subplots()
-# index = np.arange(n_groups)
-# bar_width = 0.25
-# opacity = 0.7
-#
-# rects1 = plt.bar(index, means_obs, bar_width,
-# alpha=opacity,
-# color='black',
-# label='Observed')
-#
-# rects2 = plt.bar(index + bar_width, means_shm2, bar_width,
-# alpha=opacity,
-# color='#1F78B5',
-# label='SHM 2')
-#
-# rects2 = plt.bar(index + bar_width*2, means_shm3, bar_width,
-# alpha=opacity,
-# color='#33A12B',
-# label='SHM 3')
-# #1F78B5 blue
-# #33A12B green
-# plt.xlabel('Year')
-# plt.ylabel('Ratio of surface to groundwater')
-# #plt.title('Scores by person')
-# plt.xticks(index + bar_width, ('2001-2002', '2002-2003', '2005-2006'))
-# plt.legend()
-#
-# plt.tight_layout()
-# plt.savefig('valid1.svg')
-
 #################################################################
-#updated plot into two panels
-# data to plot
+# data to plot Ratio of area under irrigation from surface water to groundwater
 n_groups = 3
-means_obs = (1.03, 0.12, 3.56)
+means_obs = (1.03, 0.12, 3.56) #define data
 
 # create plot
 fig, ax = plt.subplots()
@@ -62,11 +24,10 @@ plt.xticks(index, ('2001-2002', '2002-2003', '2005-2006'))
 plt.legend()
 
 plt.tight_layout()
-plt.show()
-#plt.savefig('validc.svg')
+plt.savefig('validc.svg')
 ####################################################################################
 n_groups = 3
-means_shm2 = (6.08, 3.02, 26.07)
+means_shm2 = (6.08, 3.02, 26.07) #define data
 means_shm3 = (6.08, 3.02, 24.82)
 
 # create plot
@@ -93,5 +54,4 @@ plt.xticks(index + np.repeat(bar_width,3)/2, ('2001-2002', '2002-2003', '2005-20
 plt.legend()
 
 plt.tight_layout()
-plt.show()
-#plt.savefig('validd.svg')
+plt.savefig('validd.svg')
