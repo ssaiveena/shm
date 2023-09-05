0443f58337d52654cdd574a6e018e3f5e916e026
Sai Veena <40389169+ssaiveena@users.noreply.github.com>
2023-09-05 12:29:44 -0700

    Update main.m

--- main.m
+++ main.m
@@ -25,23 +25,13 @@ PE_CA2 = FN_PE(419:1426+24*3);%extract data from 1968 to 2013
 PE_CA2_month = sum(reshape(PE_CA2,2,[]));
 
 %run model for SHM1
-result1 = eval_objs_GW_Recipient_CLD1(Q2_month,n_year,rainfall_CA2_month,PE_CA2_month);
-stor(:,1) = result1.storage(381:540)';
-result=result1;
+result = eval_objs_GW_Recipient_CLD1(Q2_month,n_year,rainfall_CA2_month,PE_CA2_month);
 save resultcld1 result
 
 %run model for SHM2
-result2 = eval_objs_GW_Recipient_CLD2(Q2_month,n_year,rainfall_CA2_month,PE_CA2_month);
-stor(:,2) = result2.storage(381:540)';
-gw (:,1) = result2.GW(467:540)';
-result=result2;
+result = eval_objs_GW_Recipient_CLD2(Q2_month,n_year,rainfall_CA2_month,PE_CA2_month);
 save resultcld2 result
 
 %run model for SHM3
-result3 = eval_objs_GW_Recipient_CLD3(Q2_month,n_year,rainfall_CA2_month,PE_CA2_month);
-stor(:,3) = result3.storage(381:540)';
-gw (:,2) = result3.GW(467:540)';
-result=result3;
+result = eval_objs_GW_Recipient_CLD3(Q2_month,n_year,rainfall_CA2_month,PE_CA2_month);
 save resultcld3 result
-result.date = Data.Q(25:2:1104,1:2);
-
